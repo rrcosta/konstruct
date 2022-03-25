af842855d5af0b00c7a06bac82ee7ed0a1d5c4d2
Rafael <rafaelrezendecosta@gmail.com>
2022-03-25 06:53:46 -0300

    chores: creates basic rspec person tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+
+    describe 'document field' do
+      context 'valid' do
+        it 'with valid content' do
+          person = FactoryBot.build(:person, document: FFaker::IdentificationBR.cpf )
+
+          expect(person.valid?).to be_truthy
+        end
+      end
+
+      context 'invalid' do
+        it 'document nil' do
+          person = FactoryBot.build(:person, document: nil)
+
+          expect(person.valid?).to be_falsey
+        end
+
+        it 'format invalid' do
+          person = FactoryBot.build(:person, document: 'ABC')
+
+          expect(person.valid?).to be_falsey
+        end
+
+        it 'blank content' do
+          person = FactoryBot.build(:person, document: '')
+
+          expect(person.valid?).to be_falsey
+        end
+      end
+    end
+
+  end
 end
