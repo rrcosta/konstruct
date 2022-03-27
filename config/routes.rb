058dc8552b2f460b5fd17c4edadab4130e82fe5a
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2022-03-27 10:39:13 -0300

    chore: Adds public controllers and view (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'public/index'
   resources :people
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'public#index'
 end
