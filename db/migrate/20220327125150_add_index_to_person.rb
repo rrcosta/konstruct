5a571367d1a6bc64d3148fefba85e40680711097
Rafael <rafaelrezendecosta@gmail.com>
2022-03-27 09:59:44 -0300

    fix: Removes unecessary adds columsn name and document columns

--- db/migrate/20220327125150_add_index_to_person.rb
+++ db/migrate/20220327125150_add_index_to_person.rb
@@ -1,8 +1,6 @@
 class AddIndexToPerson < ActiveRecord::Migration[7.0]
   def change
-    add_column :people, :name, :string
     add_index :people, :name
-    add_column :people, :document, :string
     add_index :people, :document
   end
 end
