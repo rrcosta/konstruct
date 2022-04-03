6b1893bdbff71308ec8596cf8f6bb94c11eb9faa
Rafael <rafaelrezendecosta@gmail.com>
2022-04-03 15:51:00 -0300

    chore: adds default value at data_address

--- db/migrate/20220321214833_create_people.rb
+++ db/migrate/20220321214833_create_people.rb
@@ -5,7 +5,7 @@ class CreatePeople < ActiveRecord::Migration[7.0]
       t.string :kind_document, default: nil
       t.string :document, default: nil
       t.string :email, default: nil
-      t.jsonb :data_address
+      t.jsonb :data_address, default: nil
 
       t.timestamps
     end
