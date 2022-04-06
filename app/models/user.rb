037af7d6a6cb4136874c5c160b606408055c7b0f
Rafael <rafaelrezendecosta@gmail.com>
2022-04-06 15:18:38 -0300

    chore: Adds User validations at Name and Email fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum:3 }
+  validates :email, presence: true, length: { minimum:5 }
 end
